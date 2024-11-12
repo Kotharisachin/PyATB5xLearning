4fafc9f9e1c870bb7a707dfeebe8fd37420e109c
Sachin <sachinkotharii17@gmail.com>
2024-11-12 07:46:52 +0530

    Added 12th Nov files

---
+++ November/ex_12-11-2024_Operators/Lab031_Operators.py
@@ -0,0 +1,15 @@
+age = 65
+#age -> Variable name
+#= -> Assignment operator
+#65-> literal
+
+#unary operator - which is added to one operand
+age1 = +65
+age2 = -65
+
+
+print(age1)
+print(age2)
+
+
+
